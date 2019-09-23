0a8968f39b1d95bc940500758c9c496a11d7ab8c
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2019-09-23 09:13:24 +0200

    Added a button and strings

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button enKnap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        enKnap = findViewById(R.id.button1);
+
+        enKnap.setOnClickListener(this);
+
     }
+
+    public void onClick(View v) {
+        // Vis et tal der skifter så vi kan se hver gang der trykkes
+        long etTal = System.currentTimeMillis();
+
+        // Skriv meddelelse på knappen (kan på skærmen)
+        enKnap.setText("Du trykkede på mig. Tak! \n" + etTal);
+
+        // Skriv meddelelse til loggen (loggen kan ses med adb logcat)
+        System.out.println("Der blev trykket på knappen");
+
+        Log.d("BenytEnKnap", "Knap trykket");
+    }
+
 }
