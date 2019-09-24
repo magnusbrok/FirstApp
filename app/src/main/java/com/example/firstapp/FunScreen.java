5d155b51d0763515727ec4e2be64d415883dd1cb
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2019-09-24 12:33:15 +0200

    started on webview

--- app/src/main/java/com/example/firstapp/FunScreen.java
+++ app/src/main/java/com/example/firstapp/FunScreen.java
@@ -1,26 +1,20 @@
 package com.example.firstapp;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class FunScreen extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fun_screen);
+        WebView webView = new WebView(this);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://javabog.dk");
+        setContentView(webView);
     }
-
-    public class BenytWebView1 extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            WebView webView = new WebView(this);
-            webView.loadUrl("http://javabog.dk");
-            setContentView(webView);
-        }
-    }
-
 }
