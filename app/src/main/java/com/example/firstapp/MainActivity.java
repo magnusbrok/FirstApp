a7a039d6bc283fbcfc88cba85e921d130d0ddc81
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2019-09-26 13:11:36 +0200

    Added userInput to find username

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,26 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
 
     Button enKnap, enKnap2;
+    TextView userInfo;
+    EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        userInfo = findViewById(R.id.textView2);
+        userInput = findViewById(R.id.editText);
         enKnap = findViewById(R.id.button1);
         enKnap2 = findViewById(R.id.button2);
 
         enKnap.setOnClickListener(this);
         enKnap2.setOnClickListener(this);
+        userInput.setOnEditorActionListener(this);
     }
 
     public void onClick(View v) {
@@ -38,7 +43,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == enKnap2){
             enKnap2.setText("Går til google.com");
             openActivity();
-
         }
 
         // Skriv meddelelse til loggen (loggen kan ses med adb logcat)
@@ -52,4 +56,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if (v == userInput) {
+            String userName = userInput.getText().toString();
+            userInfo.setText("User: " + userName);
+
+        }
+        return true;
+    }
 }
